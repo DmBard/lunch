cd011c5f256b350b10f95ed5eaeb4ead78e92ee8
d.baryshev <dm.baryshev@gmail.com>
2015-05-25 18:54:48 +0300

    add easy admin bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Ens\LunchBundle\EnsLunchBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
