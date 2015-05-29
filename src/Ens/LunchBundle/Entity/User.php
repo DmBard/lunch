66c6793012cd38a6c54a6f45642ff3224ab4fcc5
d.baryshev <dm.baryshev@gmail.com>
2015-05-29 18:39:09 +0300

    add ldap auth (ne ochen huevaya version)

--- src/Ens/LunchBundle/Entity/User.php
+++ src/Ens/LunchBundle/Entity/User.php
@@ -5,7 +5,6 @@ namespace Ens\LunchBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use FOS\UserBundle\Model\User as BaseUser;
 use FR3D\LdapBundle\Model\LdapUserInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * User
@@ -78,56 +77,22 @@ class User extends BaseUser implements LdapUserInterface
     }
 
     /**
-     * The equality comparison should neither be done by referential equality
-     * nor by comparing identities (i.e. getId() === getId()).
+     * Set Ldap Distinguished Name.
      *
-     * However, you do not need to compare every attribute, but only those that
-     * are relevant for assessing whether re-authentication is required.
-     *
-     * Also implementation should consider that $user instance may implement
-     * the extended user interface `AdvancedUserInterface`.
-     *
-     * @param UserInterface $user
-     *
-     * @return bool
+     * @param string $dn Distinguished Name
      */
-    public function isEqualTo(UserInterface $user)
-    {
-        // TODO: Implement isEqualTo() method.
-    }
-
-    public function getDn()
-    {
-        return $this->dn;
-    }
-
     public function setDn($dn)
     {
         $this->dn = $dn;
     }
 
-    public function getCn()
+    /**
+     * Get Ldap Distinguished Name.
+     *
+     * @return string Distinguished Name
+     */
+    public function getDn()
     {
-        // TODO: Implement getCn() method.
-    }
-
-    public function setCn($cn)
-    {
-        // TODO: Implement setCn() method.
-    }
-
-    public function getAttributes()
-    {
-        // TODO: Implement getAttributes() method.
-    }
-
-    public function setAttributes(array $attributes)
-    {
-        // TODO: Implement setAttributes() method.
-    }
-
-    public function getAttribute($name)
-    {
-        // TODO: Implement getAttribute() method.
+        return $this->dn;
     }
 }
