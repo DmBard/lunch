55fdc8c24a24a277800e934a3000b2ec58b2b692
Dmitriy Baryshev <dm.baryshev@gmail.com>
2015-06-16 16:30:10 +0300

    some improvements

--- web/index.php
+++ web/index.php
@@ -1,23 +1,39 @@
 <?php
-// используется ldap-привязка
-//$ldaprdn  = 'CN=Барышев Дмитрий Анатольевич,OU=Users,OU=R4S,OU=SVRD-44-B,OU=SPB,OU=RU,OU=Offices,DC=tps,DC=local';     // ldap rdn или dn
-$ldaprdn = 'tps\d.baryshev';     // ldap rdn или dn
-$ldappass = 'tanya220964';  // ассоциированный пароль
+//// используется ldap-привязка
+////$ldaprdn  = 'CN=Барышев Дмитрий Анатольевич,OU=Users,OU=R4S,OU=SVRD-44-B,OU=SPB,OU=RU,OU=Offices,DC=tps,DC=local';     // ldap rdn или dn
+//$ldaprdn = 'tps\d.baryshev';     // ldap rdn или dn
+//$ldappass = 'tanya220964';  // ассоциированный пароль
+//
+//// соединение с сервером
+//$ldapconn = ldap_connect("tps.local") or die("NO");
+//ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
+//
+//if ($ldapconn) {
+//
+//    // привязка к ldap-серверу
+//    $ldapbind = ldap_bind($ldapconn, $ldaprdn, $ldappass);
+//
+//    // проверка привязки
+//    if ($ldapbind) {
+//        echo "good";
+//    } else {
+//        echo "bad";
+//    }
+//}
 
-// соединение с сервером
-$ldapconn = ldap_connect("tps.local") or die("NO");
-ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
 
-if ($ldapconn) {
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
 
-    // привязка к ldap-серверу
-    $ldapbind = ldap_bind($ldapconn, $ldaprdn, $ldappass);
+class NSH extends \SessionHandler {
+    public function a()
+    {
+        return 1;
 
-    // проверка привязки
-    if ($ldapbind) {
-        echo "good";
-    } else {
-        echo "fuck";
     }
 }
-?>
+
+$a = new NSH();
+
+echo $a->a();
+
